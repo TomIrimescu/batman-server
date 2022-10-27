88f8d2cf78ef01c0386fe732ef2b2d3301ee3943
Tom Irimescu <tom@irimescu.com>
2022-10-27 12:19:59 -0400

    Initial code commit

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import "reflect-metadata";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+
+import { BookResolver } from "./resolvers/BookResolver";
+import { myDataSource } from "../app-data-source";
+
+require("dotenv").config();
+
+import cors from "cors";
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200 
+}
+
+async function main() {
+// establish database connection
+myDataSource
+    .initialize()
+    .then(() => {
+        console.log("\n 😻 Database connection is active!")
+    })
+    .catch((err:any) => {
+        console.error("Error during Data Source initialization:", err)
+    })
+
+  const schema = await buildSchema({ resolvers: [BookResolver] });
+
+  const server = new ApolloServer({ schema });
+  await server.start();
+  
+  const app = express();
+  app.use(cors(corsOptions));
+
+  server.applyMiddleware({ app });
+  
+  app.listen({ port: process.env.SERVER_PORT }, () =>
+    console.log(`\n 🚀 Server ready at http://localhost:${process.env.SERVER_PORT}${server.graphqlPath}`)
+  );    
+}
+
+main();
